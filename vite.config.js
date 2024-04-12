73858c56aa65e645bbd1a52737e23b140bfc40b7
Vncntkyl <kyle.rinoza2009@gmail.com>
2024-04-12 15:35:12 +0800

    feat: install xlsx

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { ViteAliases } from "vite-aliases";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  assetsInclude: ["**/*.xlsx"],
   plugins: [react(), ViteAliases()],
   server: {
     port: 5174,
